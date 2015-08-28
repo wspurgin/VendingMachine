1784427752c85f284309ac6cc1b3a861ed8a598f
Will Spurgin <will.spurgin@gmail.com>
2015-08-28 09:48:57 -0500

    Updated Schema to match Rails conventions
    Also added Rspec to project

--- db/migrate/20150827171628_create_database.rb
+++ db/migrate/20150827171628_create_database.rb
@@ -1,6 +1,6 @@
 class CreateDatabase < ActiveRecord::Migration
   def change
-    create_table "Group_Permissions", id: false, force: :cascade do |t|
+    create_table "group_permissions", id: false, force: :cascade do |t|
       t.integer "group_id",      limit: 4, default: 0, null: false
       t.integer "permission_id", limit: 4, default: 0, null: false
     end
@@ -8,11 +8,11 @@ class CreateDatabase < ActiveRecord::Migration
     add_index "group_permissions", ["group_id"], name: "group_id", using: :btree
     add_index "group_permissions", ["permission_id"], name: "permission_id", using: :btree
 
-    create_table "Groups", force: :cascade do |t|
+    create_table "groups", force: :cascade do |t|
       t.string "name", limit: 30, null: false
     end
 
-    create_table "Logs", force: :cascade do |t|
+    create_table "logs", force: :cascade do |t|
       t.integer "user_id",        limit: 4, null: false
       t.integer "product_id",     limit: 4, null: false
       t.integer "machine_id",     limit: 4, null: false
@@ -23,7 +23,7 @@ class CreateDatabase < ActiveRecord::Migration
     add_index "logs", ["product_id"], name: "product_id", using: :btree
     add_index "logs", ["user_id"], name: "user_id", using: :btree
 
-    create_table "Machine_Supplies", id: false, force: :cascade do |t|
+    create_table "machine_supplies", id: false, force: :cascade do |t|
       t.integer "machine_id", limit: 4,             null: false
       t.integer "product_id", limit: 4,             null: false
       t.integer "quantity",   limit: 4, default: 0, null: false
@@ -32,23 +32,23 @@ class CreateDatabase < ActiveRecord::Migration
     add_index "machine_supplies", ["machine_id"], name: "machine_id", using: :btree
     add_index "machine_supplies", ["product_id"], name: "product_id", using: :btree
 
-    create_table "Machines", force: :cascade do |t|
+    create_table "machines", force: :cascade do |t|
       t.string "machine_location", limit: 30, null: false
     end
 
-    create_table "Permissions", force: :cascade do |t|
+    create_table "permissions", force: :cascade do |t|
       t.string "description", limit: 80, null: false
       t.string "code_name",   limit: 20, null: false
     end
 
-    create_table "Products", force: :cascade do |t|
+    create_table "products", force: :cascade do |t|
       t.string "sku",    limit: 128, default: "",  null: false
       t.string "name",   limit: 50,                null: false
       t.string "vendor", limit: 30,                null: false
       t.float  "cost",   limit: 53,  default: 0.0, null: false
     end
 
-    create_table "Team_Members", id: false, force: :cascade do |t|
+    create_table "team_members", id: false, force: :cascade do |t|
       t.integer "team_id", limit: 4, default: 0, null: false
       t.integer "user_id", limit: 4, default: 0, null: false
     end
@@ -56,14 +56,14 @@ class CreateDatabase < ActiveRecord::Migration
     add_index "team_members", ["team_id"], name: "team_id", using: :btree
     add_index "team_members", ["user_id"], name: "user_id", using: :btree
 
-    create_table "Teams", force: :cascade do |t|
+    create_table "teams", force: :cascade do |t|
       t.string "team_name",       limit: 30,               null: false
       t.string "class",           limit: 30,               null: false
       t.date   "expiration_date",                          null: false
       t.float  "team_balance",    limit: 53, default: 0.0, null: false
     end
 
-    create_table "User_Permissions", id: false, force: :cascade do |t|
+    create_table "user_permissions", id: false, force: :cascade do |t|
       t.integer "user_id",       limit: 4, default: 0, null: false
       t.integer "permission_id", limit: 4, default: 0, null: false
     end
@@ -71,7 +71,8 @@ class CreateDatabase < ActiveRecord::Migration
     add_index "user_permissions", ["permission_id"], name: "permission_id", using: :btree
     add_index "user_permissions", ["user_id"], name: "user_id", using: :btree
 
-    create_table "Users", force: :cascade do |t|
+    create_table "users", force: :cascade do |t|
+      t.string  "smu_id",   limit: 32,                null: false
       t.string  "password", limit: 128,               null: false
       t.string  "name",     limit: 30,                null: false
       t.string  "email",    limit: 64,                null: false
@@ -80,18 +81,19 @@ class CreateDatabase < ActiveRecord::Migration
     end
 
     add_index "users", ["group_id"], name: "group", using: :btree
+    add_index "users", ["smu_id"], name: "smu_id", using: :btree
 
-    add_foreign_key "Group_Permissions", "Groups", column: "group_id", name: "group_permission_fk_group", on_update: :cascade, on_delete: :cascade
-    add_foreign_key "Group_Permissions", "Permissions", column: "permission_id", name: "group_permission_fk_permission", on_update: :cascade, on_delete: :cascade
-    add_foreign_key "Logs", "Machines", column: "machine_id", name: "log_fk_machine", on_update: :cascade, on_delete: :cascade
-    add_foreign_key "Logs", "Products", column: "product_id", name: "log_fk_product", on_update: :cascade, on_delete: :cascade
-    add_foreign_key "Logs", "Users", column: "user_id", name: "log_fk_user", on_update: :cascade, on_delete: :cascade
-    add_foreign_key "Machine_Supplies", "Machines", column: "machine_id", name: "machine_supplies_fk_machine", on_update: :cascade, on_delete: :cascade
-    add_foreign_key "Machine_Supplies", "Products", column: "product_id", name: "machine_supplies_fk_product", on_update: :cascade, on_delete: :cascade
-    add_foreign_key "Team_Members", "Teams", column: "team_id", name: "team_members_fk_team", on_update: :cascade, on_delete: :cascade
-    add_foreign_key "Team_Members", "Users", column: "user_id", name: "team_members_fk_user", on_update: :cascade, on_delete: :cascade
-    add_foreign_key "User_Permissions", "Permissions", column: "permission_id", name: "user_permission_fk_permission", on_update: :cascade, on_delete: :cascade
-    add_foreign_key "User_Permissions", "Users", column: "user_id", name: "user_permission_fk_group", on_update: :cascade, on_delete: :cascade
-    add_foreign_key "Users", "Groups", column: "group_id", name: "user_fk_group", on_update: :cascade, on_delete: :cascade
+    add_foreign_key "group_permissions", "groups", column: "group_id", name: "group_permission_fk_group", on_update: :cascade, on_delete: :cascade
+    add_foreign_key "group_permissions", "permissions", column: "permission_id", name: "group_permission_fk_permission", on_update: :cascade, on_delete: :cascade
+    add_foreign_key "logs", "machines", column: "machine_id", name: "log_fk_machine", on_update: :cascade, on_delete: :cascade
+    add_foreign_key "logs", "products", column: "product_id", name: "log_fk_product", on_update: :cascade, on_delete: :cascade
+    add_foreign_key "logs", "users", column: "user_id", name: "log_fk_user", on_update: :cascade, on_delete: :cascade
+    add_foreign_key "machine_supplies", "machines", column: "machine_id", name: "machine_supplies_fk_machine", on_update: :cascade, on_delete: :cascade
+    add_foreign_key "machine_supplies", "products", column: "product_id", name: "machine_supplies_fk_product", on_update: :cascade, on_delete: :cascade
+    add_foreign_key "team_members", "teams", column: "team_id", name: "team_members_fk_team", on_update: :cascade, on_delete: :cascade
+    add_foreign_key "team_members", "users", column: "user_id", name: "team_members_fk_user", on_update: :cascade, on_delete: :cascade
+    add_foreign_key "user_permissions", "permissions", column: "permission_id", name: "user_permission_fk_permission", on_update: :cascade, on_delete: :cascade
+    add_foreign_key "user_permissions", "users", column: "user_id", name: "user_permission_fk_group", on_update: :cascade, on_delete: :cascade
+    add_foreign_key "users", "groups", column: "group_id", name: "user_fk_group", on_update: :cascade, on_delete: :cascade
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150827171628) do
 
-  create_table "Group_Permissions", id: false, force: :cascade do |t|
+  create_table "group_permissions", id: false, force: :cascade do |t|
     t.integer "group_id",      limit: 4, default: 0, null: false
     t.integer "permission_id", limit: 4, default: 0, null: false
   end
@@ -21,11 +21,11 @@ ActiveRecord::Schema.define(version: 0) do
   add_index "group_permissions", ["group_id"], name: "group_id", using: :btree
   add_index "group_permissions", ["permission_id"], name: "permission_id", using: :btree
 
-  create_table "Groups", force: :cascade do |t|
+  create_table "groups", force: :cascade do |t|
     t.string "name", limit: 30, null: false
   end
 
-  create_table "Logs", force: :cascade do |t|
+  create_table "logs", force: :cascade do |t|
     t.integer "user_id",        limit: 4, null: false
     t.integer "product_id",     limit: 4, null: false
     t.integer "machine_id",     limit: 4, null: false
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 0) do
   add_index "logs", ["product_id"], name: "product_id", using: :btree
   add_index "logs", ["user_id"], name: "user_id", using: :btree
 
-  create_table "Machine_Supplies", id: false, force: :cascade do |t|
+  create_table "machine_supplies", id: false, force: :cascade do |t|
     t.integer "machine_id", limit: 4,             null: false
     t.integer "product_id", limit: 4,             null: false
     t.integer "quantity",   limit: 4, default: 0, null: false
@@ -45,23 +45,23 @@ ActiveRecord::Schema.define(version: 0) do
   add_index "machine_supplies", ["machine_id"], name: "machine_id", using: :btree
   add_index "machine_supplies", ["product_id"], name: "product_id", using: :btree
 
-  create_table "Machines", force: :cascade do |t|
+  create_table "machines", force: :cascade do |t|
     t.string "machine_location", limit: 30, null: false
   end
 
-  create_table "Permissions", force: :cascade do |t|
+  create_table "permissions", force: :cascade do |t|
     t.string "description", limit: 80, null: false
     t.string "code_name",   limit: 20, null: false
   end
 
-  create_table "Products", force: :cascade do |t|
+  create_table "products", force: :cascade do |t|
     t.string "sku",    limit: 128, default: "",  null: false
     t.string "name",   limit: 50,                null: false
     t.string "vendor", limit: 30,                null: false
     t.float  "cost",   limit: 53,  default: 0.0, null: false
   end
 
-  create_table "Team_Members", id: false, force: :cascade do |t|
+  create_table "team_members", id: false, force: :cascade do |t|
     t.integer "team_id", limit: 4, default: 0, null: false
     t.integer "user_id", limit: 4, default: 0, null: false
   end
@@ -69,14 +69,14 @@ ActiveRecord::Schema.define(version: 0) do
   add_index "team_members", ["team_id"], name: "team_id", using: :btree
   add_index "team_members", ["user_id"], name: "user_id", using: :btree
 
-  create_table "Teams", force: :cascade do |t|
+  create_table "teams", force: :cascade do |t|
     t.string "team_name",       limit: 30,               null: false
     t.string "class",           limit: 30,               null: false
     t.date   "expiration_date",                          null: false
     t.float  "team_balance",    limit: 53, default: 0.0, null: false
   end
 
-  create_table "User_Permissions", id: false, force: :cascade do |t|
+  create_table "user_permissions", id: false, force: :cascade do |t|
     t.integer "user_id",       limit: 4, default: 0, null: false
     t.integer "permission_id", limit: 4, default: 0, null: false
   end
@@ -84,7 +84,8 @@ ActiveRecord::Schema.define(version: 0) do
   add_index "user_permissions", ["permission_id"], name: "permission_id", using: :btree
   add_index "user_permissions", ["user_id"], name: "user_id", using: :btree
 
-  create_table "Users", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
+    t.string  "smu_id",   limit: 32,                null: false
     t.string  "password", limit: 128,               null: false
     t.string  "name",     limit: 30,                null: false
     t.string  "email",    limit: 64,                null: false
@@ -93,17 +94,18 @@ ActiveRecord::Schema.define(version: 0) do
   end
 
   add_index "users", ["group_id"], name: "group", using: :btree
+  add_index "users", ["smu_id"], name: "smu_id", using: :btree
 
-  add_foreign_key "Group_Permissions", "Groups", column: "group_id", name: "group_permission_fk_group", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "Group_Permissions", "Permissions", column: "permission_id", name: "group_permission_fk_permission", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "Logs", "Machines", column: "machine_id", name: "log_fk_machine", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "Logs", "Products", column: "product_id", name: "log_fk_product", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "Logs", "Users", column: "user_id", name: "log_fk_user", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "Machine_Supplies", "Machines", column: "machine_id", name: "machine_supplies_fk_machine", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "Machine_Supplies", "Products", column: "product_id", name: "machine_supplies_fk_product", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "Team_Members", "Teams", column: "team_id", name: "team_members_fk_team", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "Team_Members", "Users", column: "user_id", name: "team_members_fk_user", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "User_Permissions", "Permissions", column: "permission_id", name: "user_permission_fk_permission", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "User_Permissions", "Users", column: "user_id", name: "user_permission_fk_group", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "Users", "Groups", column: "group_id", name: "user_fk_group", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "group_permissions", "groups", name: "group_permission_fk_group", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "group_permissions", "permissions", name: "group_permission_fk_permission", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "logs", "machines", name: "log_fk_machine", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "logs", "products", name: "log_fk_product", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "logs", "users", name: "log_fk_user", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "machine_supplies", "machines", name: "machine_supplies_fk_machine", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "machine_supplies", "products", name: "machine_supplies_fk_product", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "team_members", "teams", name: "team_members_fk_team", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "team_members", "users", name: "team_members_fk_user", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "user_permissions", "permissions", name: "user_permission_fk_permission", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "user_permissions", "users", name: "user_permission_fk_group", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "users", "groups", name: "user_fk_group", on_update: :cascade, on_delete: :cascade
 end
