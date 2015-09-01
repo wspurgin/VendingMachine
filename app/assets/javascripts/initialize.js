d017099af30254a6210160a1997fef49c6f30630
Will Spurgin <will.spurgin@gmail.com>
2015-09-01 17:18:17 -0500

    Added historic Javascript and styles from old site

---
+++ app/assets/javascripts/initialize.js
@@ -0,0 +1,7 @@
+// This file contains all the initializations needed globally accross the site
+// e.g. the menu.
+
+$(document).ready(function() {
+  'use strict';
+  var menu = new mlPushMenu( document.getElementById( 'mp-menu' ), document.getElementById( 'trigger' ) );
+});
